065e6991a6b5f2658d7cb5a076479968fec3b65d
dimaslobodyan <dimaslobodian.sda@gmail.com>
2024-02-21 15:38:37 +0200

    try return list in file_datasource

--- agent/src/file_datasource.py
+++ agent/src/file_datasource.py
@@ -15,8 +15,49 @@ class FileDatasource:
         self.accelerometer_filename = accelerometer_filename
         self.gps_filename = gps_filename
 
+    """def read(self) -> list[AggregatedData]:
+        #The method returns data received from the sensors
+        accelerometer_data = self._read_accelerometer_data()
+        gps_data = self._read_gps_data()
+        
+        # Determine the minimum length of data to avoid IndexError
+        min_length = min(len(accelerometer_data), len(gps_data))
+        
+        aggregated_data_list = []
+        for i in range(min_length):
+            aggregated_data_list.append(
+                AggregatedData(
+                    accelerometer_data[i],
+                    gps_data[i],
+                    datetime.now(),
+                    config.USER_ID,
+                )
+            )
+            
+        return aggregated_data_list
+
+    def _read_accelerometer_data(self) -> list[Accelerometer]:
+        accelerometer_data = []
+        with open(self.accelerometer_filename, 'r') as file:
+            csv_reader = reader(file)
+            #next(csv_reader)  # Skip header if present
+            for row in csv_reader:
+                x, y, z = map(int, row)
+                accelerometer_data.append(Accelerometer(x, y, z))
+        return accelerometer_data
+
+    def _read_gps_data(self) -> list[Gps]:
+        gps_data = []
+        with open(self.gps_filename, 'r') as file:
+            csv_reader = reader(file)
+            #next(csv_reader)  # Skip header if present
+            for row in csv_reader:
+                longitude, latitude = map(float, row)
+                gps_data.append(Gps(longitude, latitude))
+        return gps_data"""
+
     def read(self) -> AggregatedData:
-        """The method returns data received from the sensors"""
+        #The method returns data received from the sensors
         accelerometer_data = self._read_accelerometer_data()
         gps_data = self._read_gps_data()
         return AggregatedData(
